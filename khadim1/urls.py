f7524da7efd8fc45f711a21b5dff0eef1458d4a8
Muhamed Fall <fallmuhamed@gmail.com>
2020-12-29 04:39:37 +0000

    code

--- khadim1/urls.py
+++ khadim1/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 	#url('^$', vue.accueil),
 	
     path('admin/', admin.site.urls),
-    path('boutique/', include('boutique.urls'))
+    path('/', include('boutique.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
