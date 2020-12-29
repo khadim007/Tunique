a473558c0fa782616fb4b7c13a031248cb064ef7
Muhamed Fall <fallmuhamed@gmail.com>
2020-12-29 15:34:34 +0000

    Configure PostgreSQL

--- khadim1/prod_settings.py
+++ khadim1/prod_settings.py
@@ -1,7 +1,12 @@
+
+import dj_database_url
 from .settings import *
+
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASE['default'] = dj_database_url.config()
 
 SECRET_KEY = 'f7_$#$+*8j4+9_-*waiqhdzfg#jz(!g42x4y$93tylx^qetc8g'
 
