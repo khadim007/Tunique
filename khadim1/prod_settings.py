391d1db06cb0c08757862941818f12b39b5548d7
Muhamed Fall <fallmuhamed@gmail.com>
2020-12-29 16:08:55 +0000

    Setup whitenoise for staticfiles delivery

--- khadim1/prod_settings.py
+++ khadim1/prod_settings.py
@@ -8,6 +8,9 @@ TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 SECRET_KEY = 'f7_$#$+*8j4+9_-*waiqhdzfg#jz(!g42x4y$93tylx^qetc8g'
 
 ALLOWED_HOSTS = ['khadim007.herokuapp.com']
